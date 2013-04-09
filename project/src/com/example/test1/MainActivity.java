0ec1bddef0afb2f5d272a646df7721db1084af50
funtion <shufangxie76@gmail.com>
2013-04-09 22:15:49 +0800

    多线程，模拟器可以，真机还不行

--- project/src/com/example/test1/MainActivity.java
+++ project/src/com/example/test1/MainActivity.java
@@ -4,13 +4,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -22,6 +21,7 @@ public class MainActivity extends Activity {
    EditText etCardNumber;
    EditText etPassword ;
    Button btLogin;
+   AlertDialog.Builder message; 
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,34 +34,12 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				 String sCardNumber = etCardNumber.getText().toString();
-			     String sPassword = etPassword.getText().toString();	
-			     if( sCardNumber == null || sPassword == null)
+			     String sPassword = etPassword.getText().toString();
+			     message= new  AlertDialog.Builder(MainActivity.this);
+			     if( sCardNumber != null && sPassword != null)
 			     {
-			    	 return ;
+			    	 login(sCardNumber,sPassword);
 			     }
-			     AlertDialog.Builder message = new  AlertDialog.Builder(MainActivity.this);
-			     String result = null; 
-			     try {
-			    	result = login(sCardNumber,sPassword);
-					if(result != null)
-					 {
-						
-						 String[] userInfo = result.split(","); 
-						 message.setMessage("欢迎你，"+userInfo[0]).show();
-					 }
-					else
-					{
-						message.setMessage("用户名或密码错误").show();
-					}
-				} catch (ClientProtocolException e) {
-					 message.setMessage("ClientProtocolException").show();
-//					e.printStackTrace();
-				} catch (IOException e) {
-					message.setMessage("IOException").show();
-//					e.printStackTrace();
-				}
-			     
-				
 			}
 		});
         
@@ -74,42 +52,68 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
-    private String login(String cardNumber, String password) throws ClientProtocolException, IOException
-    {
-    	class param implements NameValuePair
-    	{
-    		private String name;
-    		private String value;
-    		param(String name,String value)
-    		{
-    			this.name = name;
-    			this.value = value;
-    		}
-			@Override
-			public String getName() {
-				return name;
-			}
+    class param implements NameValuePair
+	{
+		private String name;
+		private String value;
+		param(String name,String value)
+		{
+			this.name = name;
+			this.value = value;
+		}
+		@Override
+		public String getName() {
+			return name;
+		}
 
-			@Override
-			public String getValue() {
-				return value;
+		@Override
+		public String getValue() {
+			return value;
+		}
+		
+	}
+	class LoginTask extends AsyncTask<Void,Integer,String>{
+		String url;
+		List<NameValuePair> list;
+		LoginTask(String url,List<NameValuePair> list){
+			this.url = url;
+			this.list = list;
+		}
+		@Override
+		protected String doInBackground(Void... arg0) {
+			try {
+				String result = HttpConnecter.post(url, list);
+				return result;
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+				message.setMessage("ClientProtocolException").show();
+			} catch (IOException e) {
+				e.printStackTrace();
+				message.setMessage("IOException").show();
+			} catch(Exception e){
+				e.printStackTrace();
+				message.setMessage("Error").show();
 			}
-    		
-    	}
+			return null;
+		}
+		@Override
+	    protected void onPostExecute(String result) {
+			if(result != null){
+				String[] userInfo = result.split(","); 
+				message.setMessage("欢迎你，"+userInfo[0]).show();
+			}else{
+				message.setMessage("用户名或密码错误").show();
+			}
+	    }
+		
+	}
+    private void login(String cardNumber, String password) 
+    {
     	List<NameValuePair> list = new ArrayList<NameValuePair>();
     	list.add(new param("username",cardNumber));
     	list.add(new param("password",password));
     	String url = "http://121.248.63.105:8080/authentication/";
-//    	String url = "http://www.baidu.com";
-    	String result=HttpConnecter.post(url, list); 
-//    	String result=HttpConnecter.get(url); 
-//    	if(result == null)
-//    	{
-//    		return false;
-//    	}
-//    	
-//    	return true;
-    	return result;
+    	LoginTask loginTask = new LoginTask(url,list);
+    	loginTask.execute();
     }
 }
